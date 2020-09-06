af026c0198ccc1164c29143835781feda186e36c
zoezhang926 <zoezhang926@gmail.com>
2020-09-05 22:30:21 -0400

    create the loginform

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, url_for, render_template, redirect, flash, jsonify, request
+from flask import Flask, url_for, render_template, redirect, flash, jsonify, request,session
 from flask_debugtoolbar import DebugToolbarExtension
-# from models import db,connect_db
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+# from models import db,connect_db,loginForm
+from form import LoginForm
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///djdb'
@@ -8,12 +13,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "itisasecret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+
 debug = DebugToolbarExtension(app)
 
 # connect_db(app)
 
-@app.route("/")
-def home():
-    return 'Create your own playlist'
+@app.route('/')
+def login():
+  form = LoginForm()
+  return render_template('Formlogin.html', title='Sign In', form=form)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
