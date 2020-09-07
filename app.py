83bf6aa629c280c79225a0fd6bc16ddde7189b2e
zoezhang926 <zoezhang926@gmail.com>
2020-09-06 23:01:23 -0400

    update wtform,models and app.py to enable users to make customized song list

--- app.py
+++ app.py
@@ -1,28 +1,175 @@
-from flask import Flask, url_for, render_template, redirect, flash, jsonify, request,session
+from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-# from models import db,connect_db,loginForm
-from form import LoginForm
+from werkzeug.exceptions import Unauthorized
+
+from models import connect_db, db, User, Song
+from forms import RegisterForm, LoginForm, SongForm, DeleteForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///djdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///djdb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "itisasecret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
 
-debug = DebugToolbarExtension(app)
+toolbar = DebugToolbarExtension(app)
 
-# connect_db(app)
+connect_db(app)
 
-@app.route('/')
+@app.route("/")
+def homepage():
+  """ add a nav to redirect toute"""
+  return redirect("/register")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register a user: produce form and handle form submission."""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+
+        user = User.register(username, password, first_name, last_name, email)
+
+        db.session.commit()
+        session['username'] = user.username
+
+        return redirect(f"/users/{user.username}")
+
+    else:
+        return render_template("users/register.html", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  form = LoginForm()
-  return render_template('Formlogin.html', title='Sign In', form=form)
+    """Produce login form or handle login."""
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)  # <User> or False
+        if user:
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ["Invalid username/password."]
+            return render_template("users/login.html", form=form)
+
+    return render_template("users/login.html", form=form)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username")
+    return redirect("/login")
+  
+    """Logout route."""
+
+
+
+@app.route("/users/<username>")
+def show_user(username):
+    """Example page for logged-in-users."""
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get(username)
+    form = DeleteForm()
+
+    return render_template("users/show.html", user=user, form=form)
+  
+@app.route("/users/<username>/delete", methods=["POST"])
+def remove_user(username):
+    """Remove user nad redirect to login."""
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get(username)
+    db.session.delete(user)
+    db.session.commit()
+    session.pop("username")
+
+    return redirect("/login")
+
+@app.route("/users/<username>/song/new", methods=["GET", "POST"])
+def new_song(username):
+    """Show add-song form and process it."""
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    form = SongForm()
+
+    if form.validate_on_submit():
+        cover = form.cover.data
+        title = form.title.data
+        link = form.link.data
+
+        song = Song(
+          cover = cover,
+          title = title,
+          link = link,
+          username=username,
+        )
+
+        db.session.add(song)
+        db.session.commit()
+
+        return redirect(f"/users/{song.username}")
+
+    else:
+        return render_template("song/new.html", form=form)
+      
+@app.route("/song/<int:song_id>/update", methods=["GET", "POST"])
+def update_song(song_id):
+    """Show update-song form and process it."""
+
+    song = Song.query.get(song_id)
+
+    if "username" not in session or song.username != session['username']:
+        raise Unauthorized()
+
+    form = SongForm(obj=song)
+
+    if form.validate_on_submit():
+        song.cover = form.cover.data
+        song.title = form.title.data
+        song.link = form.link.data
+
+        db.session.commit()
+
+        return redirect(f"/users/{song.username}")
+
+    return render_template("/song/edit.html", form=form, song=song)
+
+@app.route("/song/<int:song_id>/delete", methods=["POST"])
+def delete_song(song_id):
+    """Delete a song."""
+
+    song = Song.query.get(song_id)
+    if "username" not in session or song.username != session['username']:
+        raise Unauthorized()
+
+    form = DeleteForm()
+
+    if form.validate_on_submit():
+        db.session.delete(song)
+        db.session.commit()
+
+    return redirect(f"/users/{song.username}")
---
+++ forms.py
@@ -0,0 +1,59 @@
+
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
+from flask_wtf import FlaskForm
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    
+class SongForm(FlaskForm):
+    """Add feedback form."""
+    cover =StringField(
+        "cover",
+        validators=[InputRequired(), Length(max=300)],
+    )
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    link = StringField(
+        "Link",
+        validators=[InputRequired()],
+    )
+
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
